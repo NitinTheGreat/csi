b6ca0338c70e5809d6521b01ab3a08fdad0c3aa2
Nitin Kumar Pandey <83699589+NitinTheGreat@users.noreply.github.com>
2024-03-01 22:03:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentQuestion = 1;
 var questions = document.querySelectorAll('.question');
 var submitBtn = document.getElementById('submit-btn');
+var questionNumbers = document.querySelectorAll('.question-numbers li');
 
 // Show question 1 when the page loads
 window.onload = function() {
@@ -13,7 +14,7 @@ function showQuestion(questionNumber) {
     });
     document.getElementById('question' + questionNumber).style.display = 'block';
     currentQuestion = questionNumber;
-    // Show submit button if it's the last question
+
     if (currentQuestion === questions.length) {
         submitBtn.style.display = 'block';
     } else {
@@ -27,23 +28,24 @@ function submitAnswer(questionNumber) {
         var selectedValue = selectedOption.value;
         var correctAnswer = document.getElementById('question' + questionNumber + 'CorrectAnswer').value;
         if (selectedValue === correctAnswer) {
-            // Correct answer logic, you can update the score here
-            // For demonstration, let's just alert
+            
             alert("Correct answer!");
         } else {
-            // Incorrect answer logic
+            
             alert("Incorrect answer!");
         }
-        // Move to the next question
-        showQuestion(questionNumber + 1);
+        // Change color of question number to green
+        questionNumbers[questionNumber - 1].style.color = 'green';
+        // Move to the next question if it exists
+        showNextQuestion();
     } else {
         alert("Please select an option.");
     }
 }
 
+
 function submitTest() {
-    // Here you can implement logic to calculate the final score out of 10
-    // For demonstration purpose, let's just show an alert with the final score out of 10
+    
     alert("Test submitted! Your score out of 10 will be calculated here.");
 }
 
@@ -57,4 +59,8 @@ function showPreviousQuestion() {
     if (currentQuestion > 1) {
         showQuestion(currentQuestion - 1);
     }
+}
+
+function selectOption(questionId, optionValue) {
+    document.querySelector('input[name="question' + questionId + 'Option"][value="' + optionValue + '"]').checked = true;
 }
